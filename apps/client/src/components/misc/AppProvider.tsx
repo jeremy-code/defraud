1476991000eae3a0701b164e11d637b67a5832c2
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2024-05-24 08:09:12 -0700

    chore: Update AppProvider to use SWRConfig for data fetching and caching

--- apps/client/src/components/misc/AppProvider.tsx
+++ apps/client/src/components/misc/AppProvider.tsx
@@ -1,8 +1,21 @@
 "use client";
 
 import type { ReactNode } from "react";
+import ky, { type KyInstance } from "ky";
 import { ThemeProvider } from "next-themes";
+import { SWRConfig, type State } from "swr";
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <SWRConfig
+      value={{
+        fetcher: (...params: Parameters<KyInstance>) => ky(...params).json(),
+        // TODO: Switch from caching in-memory to caching in Redis.
+        provider: () => new Map<string, State>(),
+        suspense: true,
+      }}
+    >
+      <ThemeProvider>{children}</ThemeProvider>
+    </SWRConfig>
+  );
 };
